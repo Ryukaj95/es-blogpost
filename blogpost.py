08eb0058f5a47e4eb53d14cc5aca139603ad5914
Francesco Cambi <francesco100@hotmail.it>
2017-04-26 15:35:19 +0200

    Aggiunta della classe Post per la tabella del DB

--- blogpost.py
+++ blogpost.py
@@ -5,6 +5,19 @@ import peewee as p
 app = Flask(__name__)
 db = p.SqliteDatabase('posts.db')
 
+class Post(p.Model):
+    id = p.IntegerField()
+    title = p.CharField()
+    content = p.CharField()
+    data = p.CharField()
+
+    class Meta:
+        database = db
+        db_table="posts"
+
 @app.route('/')
 def home():
     return render_template('blog.html')
+
+# for f in Film.select():
+#     print ("{} {} {} {}\n".format(f.id,f.title,f.content,f.data))
