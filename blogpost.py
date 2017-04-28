5901d05caecbb6ea8d529a36b47f88ce4b6d99ac
Francesco Cambi <francesco100@hotmail.it>
2017-04-28 15:22:36 +0200

    Aggiunta la comunicazione col form html e l'inserimento dati nel db

--- blogpost.py
+++ blogpost.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import collections
 import peewee as p
+from flask_restful import Resource, Api
+
 
 app = Flask(__name__)
+api = Api(app)
 db = p.SqliteDatabase('posts.db')
+
+
+def insert_post_into_database(form_title, form_date, form_content):
+    query = Post.insert(title=form_title, data=form_date, content=form_content)
+    query.execute()
 
 
 class Post(p.Model):
@@ -18,11 +26,27 @@ class Post(p.Model):
         db_table = "posts"
 
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('blog.html', post=Post.select())
+
+
+@app.route('/submit', methods=["POST"])
+def submit():
+    print("ciao")
+    insert_post_into_database(
+        request.form['title'], request.form['date'], request.form['content'])
+    return redirect(url_for("home"))
 
 
 @app.route('/new_post')
 def new_post():
     return render_template('new_post.html')
+
+
+class NewPost(Resource):
+    def post(self):
+        print("ciao")
+
+
+api.add_resource(NewPost, '/home')
