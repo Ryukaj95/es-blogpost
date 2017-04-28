eca6700477dc3684ae90654cfc3a696fcdf61b27
Francesco Cambi <francesco100@hotmail.it>
2017-04-28 12:07:05 +0200

    Aggiunto il render_template per new_post

--- blogpost.py
+++ blogpost.py
@@ -19,3 +19,7 @@ class Post(p.Model):
 @app.route('/')
 def home():
     return render_template('blog.html', post=Post.select())
+
+@app.route('/new_post')
+def new_post():
+    return render_template('new_post.html')
